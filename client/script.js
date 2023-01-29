069a6d76c82489ad7c0c11084ffff5513852f155
Ankur <ankurgoswami718@gmail.com>
2023-01-29 12:41:12 +0530

    done

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (event) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://conversant-ai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
